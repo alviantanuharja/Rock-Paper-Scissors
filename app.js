ac1e798020dd48a63618ee7221bd17fd7ec1dc39
Alvian Tanuharja <alviantanuharja@gmail.com>
2022-09-03 05:21:21 +0700

    Added styling, icons, textbox that shows game status

--- app.js
+++ app.js
@@ -10,38 +10,68 @@ function getComputerChoice(){
     return choices[rngJesus()];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function startRound(playerSel, computerSel){
     let playSel = playerSel.toLowerCase();
     let compSel = computerSel.toLowerCase();
+    let playerScore = 0;
+    let computerScore = 0;
 
     if (playerSel == compSel){
-        return "It's a tie!";
+        document.getElementById('maintext').innerHTML = "It's a tie!";
+        return
     }
 
     switch(playSel){
         case "rock":
             switch(compSel){
                 case "paper":
-                    return "You Lose! Paper beats Rock";
+                    computerScore += 1;
+                    document.getElementById('maintext').innerHTML = "You Lose! Paper beats Rock";
+                    return;
+                    
+                    
                 case "scissors":
-                    return "You win! Rock beats Scissors";
+                    playerScore += 1;
+                    document.getElementById('maintext').innerHTML = "You win! Rock beats Scissors";
+                    return;
+                    
+                    
             }
         case "paper":
             switch(compSel){
-                case "scissors": 
-                    return "You lose! Scissors beats Paper";
+                case "scissors":
+                    computerScore += 1;
+                    document.getElementById('maintext').innerHTML = "You lose! Scissors beats Paper";
+                    return;
+                    
                 case "rock":
-                    return "You win! Paper beats Rock";
+                    playerScore += 1;
+                    document.getElementById('maintext').innerHTML = "You win! Paper beats Rock";
+                    return;
             }
         case "scissors":
             switch(compSel){
                 case "rock":
-                    return "You lose! Rock beats Scissors";
+                    computerScore += 1;
+                    document.getElementById('maintext').innerHTML = "You lose! Rock beats Scissors";
+                    return;
+                    
                 case "paper":
-                    return "You win! Scissors beats Paper";
+                    playerScore += 1;
+                    document.getElementById('maintext').innerHTML = "You win! Scissors beats Paper";
+                    return;
+                    
             }
     }
 }
+
+
+
+
+//Helper functions 
 
 //Entire form element
 const form = document.getElementById('form')
@@ -50,11 +80,27 @@ const form = document.getElementById('form')
 form.addEventListener('submit', (e) => {
     const userInput= document.getElementById('userInput').value;
     const result = startRound(userInput, getComputerChoice()); 
-    console.log(result);
+    console.log(userInput);
+    console.log(getComputerChoice());
+    
     e.preventDefault();
 })
 
 
 
+
+// function game(){
+// //Entire form element
+// const form = document.getElementById('form')
+
+// //Submit refreshes page by default
+// form.addEventListener('submit', (e) => {
+//     const userInput= document.getElementById('userInput').value;
+//     const result = startRound(userInput, getComputerChoice()); 
+    
+//     e.preventDefault();
+// })
+    
+// }
 
 
