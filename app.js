38ae66e11065c85a414913a8a960f5ef9388f31f
Alvian Tanuharja <alviantanuharja@gmail.com>
2022-09-02 17:00:31 +0700

    Working prototype

--- app.js
+++ app.js
@@ -7,8 +7,53 @@ function rngJesus(){
 }
 
 function getComputerChoice(){
-    alert("The computer has chosen " + choices[rngJesus()]);
+    return choices[rngJesus()];
 }
+
+function startRound(playerSel, computerSel){
+    let playSel = playerSel.toLowerCase();
+    let compSel = computerSel.toLowerCase();
+
+    if (playerSel == compSel){
+        return "It's a tie!";
+    }
+
+    switch(playSel){
+        case "rock":
+            switch(compSel){
+                case "paper":
+                    return "You Lose! Paper beats Rock";
+                case "scissors":
+                    return "You win! Rock beats Scissors";
+            }
+        case "paper":
+            switch(compSel){
+                case "scissors": 
+                    return "You lose! Scissors beats Paper";
+                case "rock":
+                    return "You win! Paper beats Rock";
+            }
+        case "scissors":
+            switch(compSel){
+                case "rock":
+                    return "You lose! Rock beats Scissors";
+                case "paper":
+                    return "You win! Scissors beats Paper";
+            }
+    }
+}
+
+//Entire form element
+const form = document.getElementById('form')
+
+//Submit refreshes page by default
+form.addEventListener('submit', (e) => {
+    const userInput= document.getElementById('userInput').value;
+    const result = startRound(userInput, getComputerChoice()); 
+    console.log(result);
+    e.preventDefault();
+})
+
 
 
 
